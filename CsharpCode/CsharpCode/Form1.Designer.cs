43b533837c0967a08d670b92c4be49e802e2065c
Jalal Mansoori <jalalmansoori19@gmail.com>
2020-08-03 15:58:34 +0500

    Add files via upload
    Arduino Sketch and C# windows form applications files

---
+++ CsharpCode/CsharpCode/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CsharpCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtCommands = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(96, 527);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 41);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtCommands
+            // 
+            this.txtCommands.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCommands.Location = new System.Drawing.Point(20, 52);
+            this.txtCommands.Multiline = true;
+            this.txtCommands.Name = "txtCommands";
+            this.txtCommands.ReadOnly = true;
+            this.txtCommands.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommands.Size = new System.Drawing.Size(348, 215);
+            this.txtCommands.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCommands);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(96, 216);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 286);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Voice Commands";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CsharpCode.Properties.Resources.picArduino;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(357, 527);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(121, 41);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 580);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Voice Recognition";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtCommands;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ voiceRecogArduino/voiceRecogArduino.ino
@@ -0,0 +1,82 @@
+/* This Program is about controlling leds states ON and OFF using voice recognition library of c# (using system.speech) 
+ * Exciting Part is you dont need to have any external module to transmit data to arduino because you can easily  
+ * use your builtin computer microphone or earphones microphone.   
+ *  
+ * This Program is just to give basic idea specially to beginners and then its your own creativity how you use it in a useful way.
+ * Keep Learning, Share, think and Repeat
+ * Enjoy !
+ * 
+ * By Jalal Mansoori
+ */
+
+const int blueLed=10;
+const int redLed=9;
+const int greenLed=8;
+
+char incomingData='0';
+ 
+void setup() {
+  // put your setup code here, to run once:
+//getting leds ready
+Serial.begin(9600);
+pinMode(blueLed, OUTPUT);
+pinMode(redLed, OUTPUT);
+pinMode(greenLed, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  incomingData=Serial.read();
+
+  // Switch case for controlling led in our case we have only 3 Blue, Green and Red 
+  switch(incomingData)
+  {
+      //These cases are only for state ON of led
+      // For blue led
+      case 'B':
+      digitalWrite(blueLed, HIGH);
+      break;    
+      
+      // For red led
+      case 'R':
+      digitalWrite(redLed, HIGH);
+      break;    
+      
+      // For green led
+      case 'G':
+      digitalWrite(greenLed, HIGH);
+      break;    
+
+      //These cases are  for state OFF of led and case name z , x, c are just randomly given you can also change but 
+      // make sure you change it in a c# code as well.
+      // For blue led
+      case 'Z':
+      digitalWrite(blueLed, LOW);
+      break;    
+      
+      // For red led
+      case 'X':
+       digitalWrite(redLed, LOW);
+      break;    
+      
+      // For green led
+      case 'C':
+       digitalWrite(greenLed, LOW);
+      break;
+
+      //For turning ON all leds at once :)
+      case 'V':
+      digitalWrite(blueLed, HIGH);
+      digitalWrite(redLed, HIGH);
+      digitalWrite(greenLed, HIGH);
+      break;
+
+      //For turning OFF all leds at once :)
+      case 'M':
+      digitalWrite(blueLed, LOW);
+      digitalWrite(redLed, LOW);
+      digitalWrite(greenLed, LOW);
+      break;
+      
+  }
+}
